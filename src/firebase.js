0c8b416a58ed72aa1f523167c02464f9acd29eb3
KeitaWata <s2022038@stu.musashino-u.ac.jp>
2023-06-18 23:00:13 +0900

    firebaseを使ってみる

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDuwCZeTcHXDd34cLUGueF2Z8yg3knMtP4",
+  authDomain: "web-attendance-603d0.firebaseapp.com",
+  projectId: "web-attendance-603d0",
+  storageBucket: "web-attendance-603d0.appspot.com",
+  messagingSenderId: "560664705126",
+  appId: "1:560664705126:web:11194ec9b12a6b8bca95a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
